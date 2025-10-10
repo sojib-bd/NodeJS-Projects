4c62ec6e9d8d0a1af1dce263ebfbaa281a062cf7
sojib-bd <sojibrahmatuzzaman@gmail.com>
2025-10-10 23:30:17 +0600

    updated the simple REST API for login and singup

--- Simple REST API/index.js
+++ Simple REST API/index.js
@@ -3,6 +3,7 @@ const app = express();
 const {Pool} = require('pg');
 const cors = require('cors');
 const path = require('path');
+const crypto = require('crypto');
 const PORT = 8081;
 
 app.use(express.json());
@@ -33,6 +34,27 @@ app.get('/customers',async (req,res)=>{
 
 });
 
+app.post('/signup',async(req,res)=>{
+    
+    try{
+        const {username,email,password} = req.body;
+        const result = await pool.query('select * from customers where username = $1',[username]);
+        if(result.rows.length > 0){
+            res.status(409).json({error: 'user already Exists'})
+        }
+        const passwordEncrypt = crypto.createHash('md5').update(password).digest('hex');
+        const newUser = await pool.query(
+        'INSERT INTO customers (email, password,username) VALUES ($1, $2, $3) RETURNING *',
+        [email,passwordEncrypt,username]
+      );
+
+      res.status(201).json({ message: 'signup has been completed'});
+    }catch(error){
+      console.error('Error saving to DB:', error);
+      res.status(500).json({ error: 'Database error' });
+    }
+})
+
 app.get('/customers/:id',async (req,res)=>{
     const id = parseInt(req.params.id);
     try{
@@ -51,12 +73,11 @@ app.get('/profile',(req,res)=>{
     res.sendFile(path.join(__dirname, 'public', 'user-profile.html'));
 })
 
-app.post('/customers',async (req,res)=>{
+app.post('/login',async (req,res)=>{
    const {email,password} = req.body;
+   const passwordEncrypt = crypto.createHash('md5').update(password).digest('hex');
     try{
-
-        const result = await pool.query('select * from customers where email = $1 and password = $2',[email,password]);
-        
+        const result = await pool.query('select * from customers where email = $1 and password = $2',[email,passwordEncrypt]);
         if(result.rows.length > 0){
             const user = result.rows[0]
             res.status(200).json({success: true,message:'login successful',username: user.username})
@@ -71,22 +92,8 @@ app.post('/customers',async (req,res)=>{
 
 })
 
-app.put('/todos/:id',(req,res)=>{
-    let todo = todos.find(t=> t.id === parseInt(req.params.id));
-    if(!todo) return res.status(404).json({error:'Todo not found'});
-    const {task,done} = req.body;
-    if(task !== undefined) todo.task = task;
-    if(done !== undefined) todo.done = done;
-    res.json(todo)
-})
 
-app.delete('/todos/:id',(req,res)=>{
-    let index = todos.findIndex(t => t.id === parseInt(req.params.id));
-    if(index == -1) return res.status(404).json({error: 'Todo not found'});
 
-    todos.splice(index,1);
-    res.status(204).send();
-})
 
 app.listen(PORT,()=>{
     console.log("server is listening to port 8081..");
